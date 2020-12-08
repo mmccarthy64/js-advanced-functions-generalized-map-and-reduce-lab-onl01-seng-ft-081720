19214cea7b407d028b2332f8df084ba010353e1f
mmccarthy64 <mmccarthy64@hotmail.com>
2020-12-08 16:03:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(src, cb){
+    let r = []
+    
+    for (let i = 0; i < src.length; i++){
+        let ele = src[i]
+        r.push(cb(ele))
+    }
+
+    return r;
+}
+
+function reduce(src, cb, start){
+    let r = (!!start) ? start : src[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < src.length; i++){
+        r = cb(src[i], r)
+    }
+
+    return r;
+}
